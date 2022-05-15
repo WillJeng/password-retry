c142d0340e9b928ca94e45db8ff0361b9dfeb615
Will Jeng <vitginboy3@yahoo.com.tw>
2022-05-15 14:55:03 +0800

    basic version2

--- password.py
+++ password.py
@@ -1,16 +1,22 @@
 password = "a123456"
 i = 3
-while True:
+while i > 0:
+	i = i - 1
 	pwd = input("請輸入登入密碼:")
 	if pwd == password:
 		print("登入成功")
 		break
 	else:
-		i = i - 1
-		print("密碼錯誤,還有", i, "次機會")
-		if i == 0:
-			break
-print("已達錯誤上限")
+		print("密碼錯誤!")
+		if i > 0:
+			print("還有", i, "次機會")
+		else:
+			print("錯誤已達上限")
+		# if i > 0:		
+		# 	print("密碼錯誤,還有", i, "次機會")
+		# elif i <= 0:
+		# 	print("密碼錯誤已達上限")
+
 
 
 	
